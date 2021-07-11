c7c9086e6c6267aff15c5faf148e26525c7e3730
Henrik Ormåsen <henrik.ormasen@cgi.com>
2021-07-11 13:26:38 +0200

    version 0.1

--- epub-fix.go
+++ epub-fix.go
@@ -26,8 +26,9 @@ func read_file(filename string) (txt string) {
 	return txt
 }
 func main() {
+	version := "0.1"
 	flag.Usage = func() {
-		usagemsg := "Fix referneces in ePub"
+		usagemsg := "epub-fix, version: " + version + " - Fix referneces in ePub"
 		fmt.Fprintf(os.Stderr, usagemsg+"%s -x=forord.xml", os.Args[0])
 		flag.PrintDefaults()
 		color.Error.Println("Prøv igjen!")
@@ -41,6 +42,7 @@ func main() {
 		flag.Usage()
 		os.Exit(1)
 	} else {
+		color.Info.Println("epub-fix version: " + version)
 		color.Info.Println("xmlfile:", *x)
 
 	}
@@ -49,12 +51,15 @@ func main() {
 	oldtxt := read_file(*x)
 	matched, _ := regexp.MatchString(`footnote_plugin_reference_`, oldtxt)
 
-	// r := regexp.MustCompile(`<p><span class="footnote_referrer"><a role="button" tabindex="0" onkeypress="footnote_moveToReference_\d+_\d+'footnote_plugin_reference_\d+_\d+_\d+';"><sup id="footnote_plugin_tooltip_\d+_\d+_\d+" class="footnote_plugin_tooltip_text">[(\d+)]</sup></a><span id="footnote_plugin_tooltip_text_\d+_\d+_\d+" class="footnote_tooltip">.* … <span class="footnote_tooltip_continue">Continue reading</span></span></span></p>`)
-
-	r := regexp.MustCompile(`<span class="footnote_referrer">.*\[(\d+)\].*id="footnote_plugin_tooltip_text_(\d+_\d+_\d+)".*</span></span></span>`)
-
+	// r := regexp.MustCompile(`(?s)<span class="footnote_referrer"><a role="button" tabindex="0" onkeypress="footnote_moveToReference_\d+_\d+\('footnote_plugin_reference_(\d+_\d+_\d+)'\);"><sup id="footnote_plugin_tooltip_\d+_\d+_\d+" class="footnote_plugin_tooltip_text">\[(\d+)\].*?<span id="footnote_plugin_tooltip_text_\d+_\d+_\d+".*?<span class="footnote_tooltip_continue">Continue reading</span></span></span>`)
+	r := regexp.MustCompile(`(?s)<span class="footnote_referrer"><a role="button" tabindex="0" onkeypress="footnote_moveToReference_\d+_\d+\('footnote_plugin_reference_(\d+_\d+_\d+)'\);"><sup id="footnote_plugin_tooltip_\d+_\d+_\d+" class="footnote_plugin_tooltip_text">\[(\d+)\].*?<span id="footnote_plugin_tooltip_text_(\d+_\d+_\d+)".*?</span></span>`)
+	r2 := regexp.MustCompile(`<a id="footnote_plugin_reference_(\d+_\d+_\d+)" class="footnote_backlink"><span class="footnote_index_arrow">↑</span>(\d+)</a>`)
+	r3 := regexp.MustCompile(`<span class="footnote_tooltip_continue">Continue reading</span>`)
 	// footnote := r.FindString(oldtxt)
-	newtxt := r.ReplaceAllString(oldtxt, `<span class="footnote_referrer"><a href="#footnote_plugin_reference_$2"><sup id="footnote_plugin_tooltip_$2" class="footnote_plugin_tooltip_text">[$1]</sup></a></span>`)
+	// newtxt := r.ReplaceAllString(oldtxt, `<span class="footnote_referrer"><a href="#footnote_plugin_reference_$1"><sup id="footnote_plugin_tooltip_$1" class="footnote_plugin_tooltip_text">[$2]</sup></a></span>`)
+	newtxt := r3.ReplaceAllString(oldtxt, ``)
+	newtxt = r.ReplaceAllString(newtxt, `<span class="footnote_referrer"><a href="#footnote_plugin_reference_$1"><sup id="footnote_plugin_tooltip_$1" class="footnote_plugin_tooltip_text">[$2]</sup></a></span>`)
+	newtxt = r2.ReplaceAllString(newtxt, `<a id="footnote_plugin_reference_$1" class="footnote_backlink" href="#footnote_plugin_tooltip_$1"><span class="footnote_index_arrow">↑</span>$2</a>`)
 
 	// color.Info.Println("Footnote: " + footnote)
 
